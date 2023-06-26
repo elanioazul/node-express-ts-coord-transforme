482850e9d8d30e9330ff50c12dd67f67380b9681
hugo.herrador <hugo.herrador@inetum.com>
2023-06-26 15:00:29 +0200

    add new endpoint to transform coord

--- src/controllers/controllers.index.ts
+++ src/controllers/controllers.index.ts
@@ -24,6 +24,23 @@ export const getInitialCoords = async  (req: Request, res: Response): Promise<Re
         return res.status(500).json('Internal Server error getting InitialCoords');
     }
 }
+export const getTransformedCoords = async  (req: Request, res: Response): Promise<Response> => {
+    try {
+        const conn = (await miPool).getConnection();
+        const result = (await conn).execute(
+            `
+            Select * from SEM_CHR_GIS.TEMP_COORDINATES_TRANSFORMED
+            `
+        )
+        console.log('number of records in Transformed coord table', (await result).rows?.length);
+        (await conn).close();
+        return res.status(200).json((await result).rows);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Internal Server error getting TransformedCoords');
+    }
+}
 
 export const insertInitialCoords = async (req: Request, res: Response) => {
     try {
@@ -46,6 +63,29 @@ export const insertInitialCoords = async (req: Request, res: Response) => {
     
         res.json({
             message: 'Initials coord added successfully',
+            body: {
+                coords: { lon, lat }
+            }
+        })
+    } catch (error) {
+        console.error('Error inserting data:', error);
+    } 
+
+};
+export const transformCoords = async (req: Request, res: Response) => {
+    try {
+        const { pairOfCoords } = req.body;
+        
+        const lon =  pairOfCoords.split(' ')[0];
+        const lat =  pairOfCoords.split(' ')[1];
+        let conn = (await miPool).getConnection();
+        let result = (await conn).execute(
+            `CALL TransformPointCoodinatesAndStore(${lon}, ${lat}, 4258);`
+        );
+        //console.log("Rows inserted: " + (await result).rowsAffected);  
+    
+        res.json({
+            message: 'Initials coord transformed successfully',
             body: {
                 coords: { lon, lat }
             }
--- src/routes/coord.routes.ts
+++ src/routes/coord.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 const router = Router();
 
-import { getInitialCoords, insertInitialCoords } from '../controllers/controllers.index';
+import { getInitialCoords, getTransformedCoords, insertInitialCoords, transformCoords } from '../controllers/controllers.index';
 
 router.get('/initials', getInitialCoords);
+router.get('/transformed', getTransformedCoords);
 router.post('/sendinitials', insertInitialCoords);
+router.post('/transform', transformCoords);
    
 
 export default router;
