e9cf083282b741d975bb63d90dac5a19f4e4bc13
hugo.herrador <hugo.herrador@inetum.com>
2023-07-24 12:35:14 +0200

    improve ABSINTERSECTEDBYPOINT procedure incrorporating abs intersection function in it

--- oracle sql/procedures.sql
+++ oracle sql/procedures.sql
@@ -203,7 +203,59 @@ CREATE OR REPLACE FUNCTION DMS_TO_DD (
     RETURN dd;
   END DMS_TO_DD;
 
+-- CHECK_INTERSECTION_WITH_ABS function
+CREATE OR REPLACE FUNCTION CHECK_INTERSECTION_WITH_ABS(
+    p_longitude NUMBER,
+    p_latitude NUMBER,
+    p_srid NUMBER
+) RETURN NUMBER AS
+    v_result NUMBER;
+BEGIN
+    SELECT COUNT(*)
+    INTO v_result
+    FROM SEM_CHR_GIS.abs_2020_etrs89
+    WHERE SDO_ANYINTERACT(
+        SDO_GEOMETRY(2001, p_srid, SDO_POINT_TYPE(p_longitude, p_latitude, NULL), NULL, NULL),
+        geom
+    ) = 'TRUE';
+
+    IF v_result > 0 THEN
+        RETURN 1;
+    ELSE
+        RETURN 0;
+    END IF;
+END;
+/
+
   -- ABSINTERSECTEDBYPOINT procedure
+-- create or replace PROCEDURE ABSINTERSECTEDBYPOINT (
+--     pLongitude IN NUMBER,
+--     pLatitude IN NUMBER,
+--     selectedSrid IN NUMBER,
+--     OUT_MESSAGE OUT VARCHAR,
+--     OUT_JSON OUT CLOB
+-- ) AS 
+-- BEGIN
+--     OUT_MESSAGE := 'ABS INTERSECTED BY POINT SUCCESS';
+--     SELECT JSON_ARRAYAGG(
+--         json_object( KEY 'codiabs' VALUE CODIABS, KEY 'nomabs' VALUE NOMABS, KEY 'codiss' VALUE CODISS, KEY 'nomss' VALUE NOMSS, KEY 'codirs' VALUE CODIRS, KEY 'nomrs' VALUE NOMRS, KEY 'codiaga' VALUE CODIAGA, KEY 'nomaga' VALUE NOMAGA, KEY 'estat' VALUE ESTAT, KEY 'observacions' VALUE OBSER)
+--         format json
+--         returning clob
+--     ) AS JSON 
+--     INTO OUT_JSON
+--     FROM SEM_CHR_GIS.abs_2020_etrs89
+--     where SDO_anyinteract(
+--         SDO_GEOMETRY( 2001, selectedSrid, SDO_POINT_TYPE(pLongitude, pLatitude, NULL), NULL, NULL),
+--         geom) = 'TRUE';
+--     DBMS_OUTPUT.PUT_LINE(OUT_JSON);
+--     EXCEPTION
+--         WHEN OTHERS THEN
+--             DBMS_OUTPUT.PUT_LINE('The occured exception is -: ' || SQLERRM || SQLCODE);
+--             OUT_MESSAGE := 'ABS INTERSECTED BY POINT FAILURE';
+--             OUT_JSON:= JSON_OBJECT();
+-- END ABSINTERSECTEDBYPOINT;
+
+  -- ABSINTERSECTEDBYPOINT procedure with CHECK_INTERSECTION_WITH_ABS incorporated
 create or replace PROCEDURE ABSINTERSECTEDBYPOINT (
     pLongitude IN NUMBER,
     pLatitude IN NUMBER,
@@ -211,26 +263,34 @@ create or replace PROCEDURE ABSINTERSECTEDBYPOINT (
     OUT_MESSAGE OUT VARCHAR,
     OUT_JSON OUT CLOB
 ) AS 
+    isABSLayerIntersected NUMBER;
 BEGIN
-    OUT_MESSAGE := 'ABS INTERSECTED BY POINT SUCCESS';
-    SELECT JSON_ARRAYAGG(
-        json_object( KEY 'codiabs' VALUE CODIABS, KEY 'nomabs' VALUE NOMABS, KEY 'codiss' VALUE CODISS, KEY 'nomss' VALUE NOMSS, KEY 'codirs' VALUE CODIRS, KEY 'nomrs' VALUE NOMRS, KEY 'codiaga' VALUE CODIAGA, KEY 'nomaga' VALUE NOMAGA, KEY 'estat' VALUE ESTAT, KEY 'observacions' VALUE OBSER)
-        format json
-        returning clob
-    ) AS JSON 
-    INTO OUT_JSON
-    FROM SEM_CHR_GIS.abs_2020_etrs89
-    where SDO_anyinteract(
-        SDO_GEOMETRY( 2001, selectedSrid, SDO_POINT_TYPE(pLongitude, pLatitude, NULL), NULL, NULL),
-        geom) = 'TRUE';
-    DBMS_OUTPUT.PUT_LINE(OUT_JSON);
+    isABSLayerIntersected := CHECK_INTERSECTION_WITH_ABS(pLongitude, pLatitude, selectedSrid);
+    IF isABSLayerIntersected > 0 THEN
+        OUT_MESSAGE := 'ABS INTERSECTED BY POINT SUCCESS';
+        SELECT JSON_ARRAYAGG(
+            json_object( KEY 'codiabs' VALUE CODIABS, KEY 'nomabs' VALUE NOMABS, KEY 'codiss' VALUE CODISS, KEY 'nomss' VALUE NOMSS, KEY 'codirs' VALUE CODIRS, KEY 'nomrs' VALUE NOMRS, KEY 'codiaga' VALUE CODIAGA, KEY 'nomaga' VALUE NOMAGA, KEY 'estat' VALUE ESTAT, KEY 'observacions' VALUE OBSER)
+            format json
+            returning clob
+        ) AS JSON 
+        INTO OUT_JSON
+        FROM SEM_CHR_GIS.abs_2020_etrs89
+        where SDO_anyinteract(
+            SDO_GEOMETRY( 2001, selectedSrid, SDO_POINT_TYPE(pLongitude, pLatitude, NULL), NULL, NULL),
+            geom) = 'TRUE';
+        DBMS_OUTPUT.PUT_LINE(OUT_JSON);
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('The occured exception is -: ' || SQLERRM || SQLCODE);
+        OUT_MESSAGE := 'Coordinates passed do not intersect ABS layer';
+        OUT_JSON:= JSON_OBJECT();
+    END IF;
+
     EXCEPTION
         WHEN OTHERS THEN
             DBMS_OUTPUT.PUT_LINE('The occured exception is -: ' || SQLERRM || SQLCODE);
             OUT_MESSAGE := 'ABS INTERSECTED BY POINT FAILURE';
             OUT_JSON:= JSON_OBJECT();
 END ABSINTERSECTEDBYPOINT;
-
 
 -- CHECK_INTERSECTION_WITH_CATALUNYAFRANJA function
 CREATE OR REPLACE FUNCTION CHECK_INTERSECTION_WITH_CAT(
--- src/controllers/controllers.index.ts
+++ src/controllers/controllers.index.ts
@@ -267,19 +267,28 @@ export const intersectAbs = async (req: Request, res: Response) => {
                 OUT_MESSAGE OUT VARCHAR,
                 OUT_JSON OUT CLOB
             ) AS 
+                isABSLayerIntersected NUMBER;
             BEGIN
-                OUT_MESSAGE := 'ABS INTERSECTED BY POINT SUCCESS';
-                SELECT JSON_ARRAYAGG(
-                    json_object( KEY 'codiabs' VALUE CODIABS, KEY 'nomabs' VALUE NOMABS, KEY 'codiss' VALUE CODISS, KEY 'nomss' VALUE NOMSS, KEY 'codirs' VALUE CODIRS, KEY 'nomrs' VALUE NOMRS, KEY 'codiaga' VALUE CODIAGA, KEY 'nomaga' VALUE NOMAGA, KEY 'estat' VALUE ESTAT, KEY 'observacions' VALUE OBSER)
-                    format json
-                    returning clob
-                ) AS JSON 
-                INTO OUT_JSON
-                FROM SEM_CHR_GIS.abs_2020_etrs89
-                where SDO_anyinteract(
-                    SDO_GEOMETRY( 2001, selectedSrid, SDO_POINT_TYPE(pLongitude, pLatitude, NULL), NULL, NULL),
-                    geom) = 'TRUE';
-                DBMS_OUTPUT.PUT_LINE(OUT_JSON);
+                isABSLayerIntersected := CHECK_INTERSECTION_WITH_ABS(pLongitude, pLatitude, selectedSrid);
+                IF isABSLayerIntersected > 0 THEN
+                    OUT_MESSAGE := 'ABS INTERSECTED BY POINT SUCCESS';
+                    SELECT JSON_ARRAYAGG(
+                        json_object( KEY 'codiabs' VALUE CODIABS, KEY 'nomabs' VALUE NOMABS, KEY 'codiss' VALUE CODISS, KEY 'nomss' VALUE NOMSS, KEY 'codirs' VALUE CODIRS, KEY 'nomrs' VALUE NOMRS, KEY 'codiaga' VALUE CODIAGA, KEY 'nomaga' VALUE NOMAGA, KEY 'estat' VALUE ESTAT, KEY 'observacions' VALUE OBSER)
+                        format json
+                        returning clob
+                    ) AS JSON 
+                    INTO OUT_JSON
+                    FROM SEM_CHR_GIS.abs_2020_etrs89
+                    where SDO_anyinteract(
+                        SDO_GEOMETRY( 2001, selectedSrid, SDO_POINT_TYPE(pLongitude, pLatitude, NULL), NULL, NULL),
+                        geom) = 'TRUE';
+                    DBMS_OUTPUT.PUT_LINE(OUT_JSON);
+                ELSE
+                    DBMS_OUTPUT.PUT_LINE('The occured exception is -: ' || SQLERRM || SQLCODE);
+                    OUT_MESSAGE := 'Coordinates passed do not intersect ABS layer';
+                    OUT_JSON:= JSON_OBJECT();
+                END IF;
+            
                 EXCEPTION
                     WHEN OTHERS THEN
                         DBMS_OUTPUT.PUT_LINE('The occured exception is -: ' || SQLERRM || SQLCODE);
@@ -288,6 +297,32 @@ export const intersectAbs = async (req: Request, res: Response) => {
             END ABSINTERSECTEDBYPOINT;
             `
         );
+        //escribo en bd la funcion que chequea interseccion con capa ABS
+        (await conn).execute(
+            `
+            CREATE OR REPLACE FUNCTION CHECK_INTERSECTION_WITH_ABS(
+                p_longitude NUMBER,
+                p_latitude NUMBER,
+                p_srid NUMBER
+            ) RETURN NUMBER AS
+                v_result NUMBER;
+            BEGIN
+                SELECT COUNT(*)
+                INTO v_result
+                FROM SEM_CHR_GIS.abs_2020_etrs89
+                WHERE SDO_ANYINTERACT(
+                    SDO_GEOMETRY(2001, p_srid, SDO_POINT_TYPE(p_longitude, p_latitude, NULL), NULL, NULL),
+                    geom
+                ) = 'TRUE';
+            
+                IF v_result > 0 THEN
+                    RETURN 1;
+                ELSE
+                    RETURN 0;
+                END IF;
+            END;
+            `
+        )
     
         let result: any;
 
